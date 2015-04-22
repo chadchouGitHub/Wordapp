ea13ce93fa462305be7a7b125ca4ef2f87c8fd5d
chadchouGitHub <chiachingchou@gmail.com>
2015-04-22 13:02:19 -0400

    Fix the problem
    yes

--- wordHelper.R
+++ wordHelper.R
@@ -5,7 +5,7 @@
 
 ###----------------------------------loading word ranking df-----------------------------------
 ### Need to make sure the RData file is in the correct directory
-<<<<<<< HEAD
+
 #load('~/word-app/data/sortedOneTwoTri.RData')
 library(RCurl)
 url2 <- getURL("https://raw.githubusercontent.com/chadchouGitHub/Wordapp/master/data/twoSorted.csv")
@@ -15,22 +15,15 @@ url3 <- getURL("https://raw.githubusercontent.com/chadchouGitHub/Wordapp/master/
 triSortedCSV<- read.csv(text = url3)
 triSorted <- triSortedCSV[,2:3]
 rm(url2,url3,twoSortedCSV,triSortedCSV)
-=======
+
 #twoSorted<- get(load('~/word-app2/data/sortedTwo.RData'))
 #triSorted<- get(load('~/word-app2/data/sortedTri.RData'))
 #twoSorted <- readRDS("~/word-app2/data/twoSorted.rds")
 #triSorted <- readRDS("~/word-app2/data/triSorted.rds")
-library(RCurl)
-url2 <- getURL("https://raw.githubusercontent.com/chadchouGitHub/Wordapp/master/data/twoSorted.csv")
-twoSortedCSV<- read.csv(text = url2)
-twoSorted <- twoSortedCSV[,2:3]
-url3 <- getURL("https://raw.githubusercontent.com/chadchouGitHub/Wordapp/master/data/triSorted.csv")
-triSortedCSV<- read.csv(text = url3)
-triSorted <- triSortedCSV[,2:3]
-rm(url2,url3,twoSortedCSV,triSortedCSV)
 
 
->>>>>>> origin/master
+
+
 
 ###----------------------------------loading word ranking df-----------------------------------
 
